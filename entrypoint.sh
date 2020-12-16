5bcb3bda45d0520d5d9e556edddd8eb300a19d8e
Amir Alian <amir.cup.2006@gmail.com>
2020-12-16 12:12:42 +0100

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,25 +9,14 @@ echo " ************** MODIFIED FILES"
 printf ${MODIFIED_FILES}
 
 printf "\n*****************************\n"
+echo $(pwd)
+printf "\n*****************************\n"
 
-PATHS=$(printf ${MODIFIED_FILES} | tr \\n '\n')
-CSV_FILES=$(grep -P '.+\.csv$' <<< $PATHS)
+#PATHS=$(printf ${MODIFIED_FILES} | tr \\n '\n')
+#CSV_FILES=$(grep -P '.+\.csv$' <<< $PATHS)
 
 
-echo "$CSV_FILES" | while read FILE ; do
-    if [[ ! -f $FILE ]]
-    then
-      # skip deleted files
-      continue
-    fi
-    ROW_COUNT=$(cat  ${FILE} | xargs -l echo | wc -l)
-    for ((n=1;n<=${ROW_COUNT};n++))
-    do
-      ROW_VALUE=$(sed -n "${n}p" ${FILE})
-      if [[ ! $ROW_VALUE =~ ^\"[^\"]+\",\"[^\"]+\"$ ]]
-      then
-        echo "line $n in ${FILE} should have exact 2 columns"
-        exit 101
-      fi
-    done
-done
+php /csv-translation-validator.php ${MODIFIED_FILES}
+
+
+exit 0
