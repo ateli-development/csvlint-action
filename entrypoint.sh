8d0d1f8858653675a994195ae7213287edd22400
Lubos Hubacek <lubos.hubacek@ateli.cz>
2022-10-25 14:52:58 +0200

    Catching PHP errors

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,17 @@
 #!/bin/bash
 
-
 ERROR=0
 
 PHP_RESULT=$(php /csv-translation-validator.php ${MODIFIED_FILES})
 
 echo "$PHP_RESULT"
 
-IS_FAILED=$(echo $PHP_RESULT | grep "failed" | wc -l)
-
-
-
-
-if [[ $IS_FAILED == 1 ]]
+if [[ $(echo "$PHP_RESULT" | grep -c "failed") -gt 0 ]]
 then
-  exit 101
+    exit 101
+elif [[ $(echo "$PHP_RESULT" | grep -c "error") -gt 0 ]]
+then
+    exit 102
 fi
 
 exit 0
